7e4c0aa6b4a58a7f93e9381e6a31ba38ec8d630a
K <K@Georg.fritz.box>
2017-10-08 21:00:00 +0200

    Binärbaum ergänzt

---
+++ AlgUndDat/src/baum/Abschluss.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baum;
+
+/**
+ *
+ * @author K
+ */
+public class Abschluss extends Baumelement{
+    public Abschluss(){
+        super();
+        wert = "abschluss";
+        links = null;
+        rechts = null;
+    }
+}
